0817a207b6fe25da305622835fe15a9c704b9373
achyutsrivastava2007@gmail.com <achyutsrivastava2007@gmail.com>
2024-07-26 14:11:24 +0530

    modified:   twentytwo.py

--- twentytwo.py
+++ twentytwo.py
@@ -4,28 +4,18 @@
 # If the user guesses the word correctly, print a congratulatory message.
 
 a="Belt"
-x=input("Enter a word:")
 p = "The word consists of 4 letters"
 opt = True
-won=False
+won = False
 while opt:
     for i in a:
+        x=input("Enter a word:")
         if x==a:
             won=True
             break
         else:
-            print(i)
-    if won == True:
-        print("WOW! u nailed it")
-    else:
-        print("😭 hard luck, so am i")
-    a = input("Do you want to continue? (y/n)")
+            print(i)                            
     if a == 'y':
         continue
     else:
         opt = False
-            
-        
-            
-
-
